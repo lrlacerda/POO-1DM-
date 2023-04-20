dcbf2f770f4981f180b5f8da2441ff0ad3b09d59
Lucas Ribeiro De Lacerda <32784630803@info.local>
2023-04-20 10:43:22 -0300

    POO 1DM Projeto Personagem

--- ProjetoClassePersonagem/Program.cs
+++ ProjetoClassePersonagem/Program.cs
@@ -1,16 +1,32 @@
 ﻿using ProjetoClassePersonagem;
 
 //instanciar um objeto da classe Personagem
-Personagem Tony = new Personagem();
+// Personagem Tony = new Personagem();
 
-//exibindo as informações do objeto
-Console.WriteLine(Tony.nome);
-Console.WriteLine(Tony.idade);
-Console.WriteLine(Tony.armadura);
-Console.WriteLine(Tony.ia);
+// //exibindo as informações do objeto
+// Console.WriteLine(Tony.nome);
+// Console.WriteLine(Tony.idade);
+// Console.WriteLine(Tony.armadura);
+// Console.WriteLine(Tony.ia);
 
-//Chamando os métodos da clesse
-Tony.Atacar();
-Console.WriteLine(Tony.Defender());
-Tony.RestaurarArmadura();
+// //Chamando os métodos da clesse
+// Tony.Atacar();
+// Console.WriteLine(Tony.Defender());
+// Tony.RestaurarArmadura();
 
+
+Personagem p1 = new Personagem();
+
+Console.Write($"Digite o nome do Personagem: ");
+p1.nome = Console.ReadLine();
+
+Console.Write($"Digite a idade do Personagem: ");
+p1.idade = int.Parse(Console.ReadLine());
+
+Console.Write($"Digite o nome da Armadura do Personagem: ");
+p1.armadura = Console.ReadLine();
+
+Console.Write($"Digite o nome da IA do Personagem: ");
+p1.ia = Console.ReadLine();
+
+Console.WriteLine($"\nNome: {p1.nome}, \nIdade: {p1.idade}, \nArmadura: {p1.armadura}, \nIA: {p1.ia}");
